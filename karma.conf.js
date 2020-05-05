7bf11b5e326b2ab499f84a93c6c6b6a9969d9d1b
KaZi <tialana@gmx.at>
2020-05-05 08:34:32 +0200

    junit karma 6

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     restartOnFileChange: true,
     junitReporter: {
       outputDir: 'test-coverage', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'coverage.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
